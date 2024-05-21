fb3d5e47d69dafe5a0e3f891065688b2943040e2
Ghiurutan-Bura Péter <50810629+gburapeter@users.noreply.github.com>
2024-05-21 11:03:09 +0200

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import { Canvas } from "@react-three/fiber";
 import { ARButton, Controllers, Hands, XR } from "@react-three/xr";
 
 import "./App.css";
+import { Panel } from "./Paneel";
 
 function App() {
 	return (
 		<>
 			<ARButton />
 			<Canvas>
-				<ambientLight />
+				<ambientLight intensity={5} color={"blue"} />
 				<XR>
 					<Controllers />
 					<Hands />
+					<Panel />
 				</XR>
 			</Canvas>
 		</>
