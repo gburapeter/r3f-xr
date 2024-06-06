3c649a58526280b34cf8239bc8935080f6ab2eb8
Ghiurutan-Bura Péter <50810629+gburapeter@users.noreply.github.com>
2024-06-06 14:32:45 +0200

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Canvas } from "@react-three/fiber";
-import { Controllers, Hands, XR, XRButton } from "@react-three/xr";
+import { ARButton, Controllers, Hands, XR, XRButton } from "@react-three/xr";
 
 import "./App.css";
 import { Panel } from "./Paneel";
@@ -8,9 +8,10 @@ import { OrbitControls } from "@react-three/drei";
 function App() {
 	return (
 		<>
-			<XRButton mode="AR" />
+			<ARButton />
+
 			<Canvas camera={{ position: [3, 3, 6] }}>
-				<XR referenceSpace="local-floor">
+				<XR>
 					<ambientLight intensity={5} color={"blue"} />
 					<Controllers />
 					<Hands />
